be99d8a4060a3cf1c2ca14a26946cdaddd420d0f
Rodrigo Viola <rodrigoviola@gmail.com>
2025-02-09 20:20:44 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -54,6 +54,7 @@
           "aws-vault"
           "awscli"
           "bat"
+          "borders"
           "cirruslabs/cli/tart"
           "cmatrix"
           "coreutils"
@@ -201,6 +202,19 @@
       ];
 
       # Customize Trackpad
+      # system.defaults.CustomUserPreferences = {
+      #   "com.apple" = {
+      #     "AppleMultitouchTrackpad.Dragging" = true;
+      #     "AppleMultitouchTrackpad.TrackpadThreeFingerDrag" = true;
+      #     "driver.AppleBluetoothMultitouch.trackpad.Dragging" = true;
+      #     "driver.AppleBluetoothMultitouch.trackpad.TrackpadThreeFingerDrag" = true;
+      #   };
+      # };
+
+      # TODO: these two options work but makes the trackpad buggy, not sure why
+      #system.defaults.trackpad.TrackpadThreeFingerDrag = false;
+      #system.defaults.trackpad.Dragging = false;
+
       #system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
       #system.defaults.NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
       #system.defaults.NSGlobalDomain."com.apple.trackpad.forceClick" = true;
