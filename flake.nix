cee4b1e5fafc42123dcf7756e75958c2685d70a9
Rodrigo Viola <rodrigoviola@gmail.com>
2025-02-09 16:37:14 -0300

    Update flake

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     home-manager.url = "github:nix-community/home-manager";
+
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
