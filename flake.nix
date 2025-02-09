db71e82ca3b28732862b3e2c870497c28a96a562
Rodrigo Viola <rodrigoviola@gmail.com>
2025-02-09 16:30:38 -0300

    Lint with Alejandra

--- flake.nix
+++ flake.nix
@@ -7,228 +7,235 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";     
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, nix-darwin, nix-homebrew, home-manager }:
-    let
-      configuration = { pkgs, config, ... }: {
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    nix-darwin,
+    nix-homebrew,
+    home-manager,
+  }: let
+    configuration = {
+      pkgs,
+      config,
+      ...
+    }: {
+      # Nix settings
+      nix.settings.experimental-features = "nix-command flakes";
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      nixpkgs.config.allowUnfree = true;
+      services.nix-daemon.enable = true;
 
-        # Nix settings
-        nix.settings.experimental-features = "nix-command flakes";
-        nixpkgs.hostPlatform = "aarch64-darwin";        
-        nixpkgs.config.allowUnfree = true;
-        services.nix-daemon.enable = true;
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;
 
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true;
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
 
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
 
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 5;
+      homebrew = {
+        enable = true;
 
-        homebrew = {
-          enable = true;
-
-          taps = [
-            "cirruslabs/cli" # Tart
-            "FelixKratz/formulae" # SketchyBar, Borders
-            "hashicorp/tap"
-            "nikitabobko/tap" # AeroSpace
-          ];
-
-          brews = [
-            "aws-vault"
-            "awscli"
-            "bat"
-            "cirruslabs/cli/tart"
-            "cmatrix"
-            "coreutils"
-            "fastfetch"
-            "gh"
-            "git"
-            "gnu-sed"
-            "gnu-tar"
-            "helm"
-            "htop"
-            "hugo"
-            "jq"
-            "k9s"
-            "kubectx"
-            "kubernetes-cli"
-            "lazygit"
-            "mpv"
-            "pwgen"
-            "telnet"
-            "terraform"
-            "tmux"
-            "tmuxinator"
-            "tree"
-            "watch"
-            "wget"
-            "yq"
-            "yt-dlp"
-            #"aria2"
-            #"borders"
-            #"curl"
-            #"eksctl"
-            #"entr"
-            #"eza"
-            #"fd"
-            #"fzf"
-            #"gnupg"
-            #"go"
-            #"helmfile"
-            #"krew"
-            #"kustomize"
-            #"lazydocker"
-            #"libpq" # PostgreSQL CLI tooling
-            #"mas"
-            #"neofetch"
-            #"neovim"
-            #"nmap"
-            #"node"
-            #"oh-my-posh"
-            #"p7zip"
-            #"ripgrep"
-            #"stow"
-            #"tpm"
-          ];
-
-          casks = [
-            "aerospace"
-            "alfred"
-            "battery"
-            "chatgpt"
-            "claude"
-            "cloudflare-warp"
-            "coconutbattery"
-            "drawio"
-            "font-ibm-plex"
-            "ghostty"
-            "google-chrome"
-            "keepingyouawake"
-            "keyboardcleantool"
-            "libreoffice"
-            "notion"
-            "sparrow"
-            "spotify"
-            "telegram"
-            "the-unarchiver"
-            "visual-studio-code"
-            "whatsapp"
-            #"anydesk"
-            #"balenaetcher"
-            #"bettertouchtool" # Installing v3 manually due to old license -- https://folivora.ai/releases/btt3.552-1692.zip
-            #"bisq"
-            #"docker"
-            #"firefox"
-            #"focus"
-            #"font-blex-mono-nerd-font" # Nerd font version of IBM Plex
-            #"font-meslo-lg-nerd-font"
-            #"iterm2"
-            #"lens"
-            #"mactex" # Used for cv.pdf, to be replaced with xu-cheng/latex-docker
-            #"microsoft-onenote"
-            #"microsoft-remote-desktop"
-            #"podman-desktop"
-            #"raspberry-pi-imager"
-            #"rectangle"
-            #"session-manager-plugin"
-            #"utm"
-            #"vmware-fusion"
-          ];
-
-           masApps = {
-             "Dark Mode for Safari" = 1397180934;
-             "Disk Speed Test" = 425264550;
-             "Kindle" = 302584613;
-             "MindNode" = 1289197285;
-             "Tabs Switcher" = 1406718335;
-             "Wipr" = 1320666476;
-           };
-
-          onActivation = {
-            autoUpdate = false;
-            cleanup = "zap";
-          };
-        };
-
-        # Enable TouchID for sudo authentication
-        security.pam.enableSudoTouchIdAuth = true;
-
-        # Customize Safari
-        system.defaults.CustomUserPreferences = {
-          "com.apple.Safari" = {
-            "AlwaysRestoreSessionAtLaunch" = true;
-            "ExtensionsEnabled" = true;
-            "SearchProviderShortName" = "DuckDuckGo";
-            "ShowFullURLInSmartSearchField" = false;
-            "ShowOverlayStatusBar" = true;
-          };
-        };
-
-        # Customize Finder
-        system.defaults.finder._FXShowPosixPathInTitle = false;
-        system.defaults.finder.AppleShowAllExtensions = true;
-        system.defaults.finder.AppleShowAllFiles = true;
-        system.defaults.finder.FXEnableExtensionChangeWarning = false;
-        system.defaults.finder.QuitMenuItem = true;
-        system.defaults.finder.ShowStatusBar = true;
-        system.defaults.finder.ShowPathbar = true;
-
-        # Customize Dock
-        system.defaults.dock.autohide = true;
-        system.defaults.dock.autohide-delay = 500.0;
-        system.defaults.dock.show-recents = false;
-        system.defaults.dock.tilesize = 40;
-        system.defaults.dock.persistent-others = [];
-        system.defaults.dock.persistent-apps = [
-          "/System/Applications/Launchpad.app"
+        taps = [
+          "cirruslabs/cli" # Tart
+          "FelixKratz/formulae" # SketchyBar, Borders
+          "hashicorp/tap"
+          "nikitabobko/tap" # AeroSpace
         ];
 
-        # Customize Trackpad
-        #system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
-        #system.defaults.NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
-        #system.defaults.NSGlobalDomain."com.apple.trackpad.forceClick" = true;
-        #system.defaults.trackpad.TrackpadRightClick = true;
-        #system.defaults.trackpad.Clicking = true;
+        brews = [
+          "aws-vault"
+          "awscli"
+          "bat"
+          "cirruslabs/cli/tart"
+          "cmatrix"
+          "coreutils"
+          "fastfetch"
+          "gh"
+          "git"
+          "gnu-sed"
+          "gnu-tar"
+          "helm"
+          "htop"
+          "hugo"
+          "jq"
+          "k9s"
+          "kubectx"
+          "kubernetes-cli"
+          "lazygit"
+          "mpv"
+          "pwgen"
+          "telnet"
+          "terraform"
+          "tmux"
+          "tmuxinator"
+          "tree"
+          "watch"
+          "wget"
+          "yq"
+          "yt-dlp"
+          #"aria2"
+          #"borders"
+          #"curl"
+          #"eksctl"
+          #"entr"
+          #"eza"
+          #"fd"
+          #"fzf"
+          #"gnupg"
+          #"go"
+          #"helmfile"
+          #"krew"
+          #"kustomize"
+          #"lazydocker"
+          #"libpq" # PostgreSQL CLI tooling
+          #"mas"
+          #"neofetch"
+          #"neovim"
+          #"nmap"
+          #"node"
+          #"oh-my-posh"
+          #"p7zip"
+          #"ripgrep"
+          #"stow"
+          #"tpm"
+        ];
 
-        # Customize Keyboard
-        system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
-        system.defaults.NSGlobalDomain.KeyRepeat = 2;
-        system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
-        system.keyboard.enableKeyMapping = true;
-        system.keyboard.swapLeftCtrlAndFn = true;
+        casks = [
+          "aerospace"
+          "alfred"
+          "battery"
+          "chatgpt"
+          "claude"
+          "cloudflare-warp"
+          "coconutbattery"
+          "drawio"
+          "font-ibm-plex"
+          "ghostty"
+          "google-chrome"
+          "keepingyouawake"
+          "keyboardcleantool"
+          "libreoffice"
+          "notion"
+          "sparrow"
+          "spotify"
+          "telegram"
+          "the-unarchiver"
+          "visual-studio-code"
+          "whatsapp"
+          #"anydesk"
+          #"balenaetcher"
+          #"bettertouchtool" # Installing v3 manually due to old license -- https://folivora.ai/releases/btt3.552-1692.zip
+          #"bisq"
+          #"docker"
+          #"firefox"
+          #"focus"
+          #"font-blex-mono-nerd-font" # Nerd font version of IBM Plex
+          #"font-meslo-lg-nerd-font"
+          #"iterm2"
+          #"lens"
+          #"mactex" # Used for cv.pdf, to be replaced with xu-cheng/latex-docker
+          #"microsoft-onenote"
+          #"microsoft-remote-desktop"
+          #"podman-desktop"
+          #"raspberry-pi-imager"
+          #"rectangle"
+          #"session-manager-plugin"
+          #"utm"
+          #"vmware-fusion"
+        ];
 
-        # Customize OS
-        networking.hostName = "MacBook-Pro";
-        time.timeZone = "America/Asuncion";
-        system.defaults.WindowManager.EnableStandardClickToShowDesktop = false;
-        system.defaults.screencapture = {
-          location = "~/Desktop/Screenshots";
+        masApps = {
+          "Dark Mode for Safari" = 1397180934;
+          "Disk Speed Test" = 425264550;
+          "Kindle" = 302584613;
+          "MindNode" = 1289197285;
+          "Tabs Switcher" = 1406718335;
+          "Wipr" = 1320666476;
+        };
+
+        onActivation = {
+          autoUpdate = false;
+          cleanup = "zap";
         };
       };
-    in
-    {
-      darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
-        modules = [
-          configuration
-          nix-homebrew.darwinModules.nix-homebrew
-          {
-            nix-homebrew = {
-              enable = true;
-              enableRosetta = true;
-              user = "rodrigo";
-            };
-          }
-        ];
+
+      # Enable TouchID for sudo authentication
+      security.pam.enableSudoTouchIdAuth = true;
+
+      # Customize Safari
+      system.defaults.CustomUserPreferences = {
+        "com.apple.Safari" = {
+          "AlwaysRestoreSessionAtLaunch" = true;
+          "ExtensionsEnabled" = true;
+          "SearchProviderShortName" = "DuckDuckGo";
+          "ShowFullURLInSmartSearchField" = false;
+          "ShowOverlayStatusBar" = true;
+        };
       };
 
-      # Expose the package set, including overlays, for convenience.
-      darwinPackages = self.darwinConfigurations."mbp".pkgs;
+      # Customize Finder
+      system.defaults.finder._FXShowPosixPathInTitle = false;
+      system.defaults.finder.AppleShowAllExtensions = true;
+      system.defaults.finder.AppleShowAllFiles = true;
+      system.defaults.finder.FXEnableExtensionChangeWarning = false;
+      system.defaults.finder.QuitMenuItem = true;
+      system.defaults.finder.ShowStatusBar = true;
+      system.defaults.finder.ShowPathbar = true;
+
+      # Customize Dock
+      system.defaults.dock.autohide = true;
+      system.defaults.dock.autohide-delay = 500.0;
+      system.defaults.dock.show-recents = false;
+      system.defaults.dock.tilesize = 40;
+      system.defaults.dock.persistent-others = [];
+      system.defaults.dock.persistent-apps = [
+        "/System/Applications/Launchpad.app"
+      ];
+
+      # Customize Trackpad
+      #system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
+      #system.defaults.NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
+      #system.defaults.NSGlobalDomain."com.apple.trackpad.forceClick" = true;
+      #system.defaults.trackpad.TrackpadRightClick = true;
+      #system.defaults.trackpad.Clicking = true;
+
+      # Customize Keyboard
+      system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
+      system.defaults.NSGlobalDomain.KeyRepeat = 2;
+      system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+      system.keyboard.enableKeyMapping = true;
+      system.keyboard.swapLeftCtrlAndFn = true;
+
+      # Customize OS
+      networking.hostName = "MacBook-Pro";
+      time.timeZone = "America/Asuncion";
+      system.defaults.WindowManager.EnableStandardClickToShowDesktop = false;
+      system.defaults.screencapture = {
+        location = "~/Desktop/Screenshots";
+      };
     };
+  in {
+    darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "rodrigo";
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."mbp".pkgs;
+  };
 }
