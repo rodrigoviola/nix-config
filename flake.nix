0fc130eafbee3f8229c0286e2c505d0ecf8f0dfe
Rodrigo Viola <rodrigoviola@gmail.com>
2025-02-10 17:35:35 -0300

    Update flake

--- flake.nix
+++ flake.nix
@@ -234,8 +234,23 @@
       system.defaults.dock.mru-spaces = false;
       system.defaults.spaces.spans-displays = true;
 
-      # Customize OS
+      # Customize Networking
+      networking.computerName = "MacBook-Pro";
       networking.hostName = "MacBook-Pro";
+      networking.dns = ["8.8.8.8" "8.8.4.4"];
+      networking.knownNetworkServices = ["Wi-Fi"];
+
+      # Customize Firewall
+      # Taken from https://github.com/LnL7/nix-darwin/issues/1243#issuecomment-2605697835
+      system.activationScripts.postUserActivation.text = ''
+        # Configure firewall
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned on
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp on
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+      '';
+
+      # Customize OS
       time.timeZone = "America/Asuncion";
       system.defaults.WindowManager.EnableStandardClickToShowDesktop = false;
       system.defaults.screencapture = {
