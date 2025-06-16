eb32209c0bc799ec7524a539aae73646217579f2
Rodrigo Viola <rodrigoviola@gmail.com>
2025-06-16 18:40:31 -0300

    Update script

--- dotfiles/displayplacer/setup_displays.sh
+++ dotfiles/displayplacer/setup_displays.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-displayplacer \
-  "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:2056x1329 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" \
-  "id:7040C8A9-D747-4F0F-9DCC-DCE9B688F81A res:2560x1440 hz:60 color_depth:8 enabled:true scaling:on origin:(1210,-1440) degree:0" \
-  "id:4FDA6F83-016F-455A-8CD8-909347A46556 res:2560x1440 hz:60 color_depth:8 enabled:true scaling:on origin:(-1350,-1440) degree:0"
+host=$(scutil --get LocalHostName)
+
+if [ "$host" = "MacBook-Pro-M1" ]; then
+  displayplacer \
+    "id:s4251086178 res:2056x1329 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" \
+    "id:s573748 res:2560x1440 hz:60 color_depth:8 enabled:true scaling:on origin:(1210,-1440) degree:0" \
+    "id:s573747 res:2560x1440 hz:60 color_depth:8 enabled:true scaling:on origin:(-1350,-1440) degree:0"
+elif [ "$host" = "MacBook-Pro-2013" ]; then
+  displayplacer \
+    "id:s0 res:1920x1200 color_depth:4 enabled:true scaling:on origin:(0,0) degree:0"
+else
+  echo "No matching hostname found. Skipping display layout."
+fi
